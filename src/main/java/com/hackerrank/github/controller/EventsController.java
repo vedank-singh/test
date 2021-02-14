35e5e4d9e1bbad4c59a5773ee81bf0a8298bf0b6
vedank-singh <79039640+vedank-singh@users.noreply.github.com>
2021-02-14 14:16:19 +0530

    Update EventsController.java

--- src/main/java/com/hackerrank/github/controller/EventsController.java
+++ src/main/java/com/hackerrank/github/controller/EventsController.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/events")
 public class EventsController {
-
+System.out.println("Hello World");
 }
